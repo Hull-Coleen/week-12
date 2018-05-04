f94a7f23221c97227de7e79120c0aa0d7288db3b
chhull <ch.hull@verizon.net>
2018-05-04 15:57:38 -0400

    small typo js file

--- web/homepageJs.js
+++ web/homepageJs.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-    document.getElementById("Me").innerHTML = "I am currently a Jumior at BYU-I." + 
+    document.getElementById("Me").innerHTML = "I am currently a Junior at BYU-I." + 
 	"I am working towards achiveing my BS in Software Enginerring. " +
 	"I have been married for 25 years and have six children. ";
 }
