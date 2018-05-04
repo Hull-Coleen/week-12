7b95c5163e91bf8858a6870ece92e817a93a7d29
chhull <ch.hull@verizon.net>
2018-05-04 15:50:44 -0400

    udated some images and added js file

--- web/homepage.php
+++ web/homepage.php
@@ -2,7 +2,7 @@
 <html lang="en-us">
 <head>
 
-<title>CS 313</title>
+<title>CS 313 Homepage</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 
@@ -15,93 +15,100 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" > </script>
 
-<script type="text/javascript" src="CS313_week02js.js"></script>
-<link rel="stylesheet" type="text/css" href="homepage_style.css">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="CS313_week02js.js"></script>
+<link rel="stylesheet" type="text/css" href="homepage_style.css">
 </head>
 <body > 
-    <h1>Coleen Hull</h1><br><br>
-  <div class="container-fluid">
+  <div class="container-fluid" id="main">
+  <h1>Coleen Hull</h1><br><br>
     <div class="row">
       <div class="col-md-4"> 
-	    <p>About Me</p>
+	    <p id="Me" >About Me</p>
+		<script>
+		  document.getElementById("Me").onclick = function() {myFunction()};
+		</script>
 	  </div>
       <div class="col-md-4">
-        <img class="img-responsive" src="/Mom.jpg" alt="Me"style="max-height:420px" >
+        <img class="img-responsive" src="C:\Users\chhul\Pictures/Mom.jpg" alt="Me"style="max-height:425px" >
       </div>
       <div class="col-md-4">
-        
-      <!-- </div>
-    </div>
-  </div> -->
-  <!-- <div class="container"> -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-		<li data-target="#myCarousel" data-slide-to="3"></li>
-        <li data-target="#myCarousel" data-slide-to="4"></li>
-		<li data-target="#myCarousel" data-slide-to="5"></li>
-      </ol>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+	    
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+		  <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
+		  <li data-target="#myCarousel" data-slide-to="5"></li>
+        </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="/Kristine.jpg" alt="Los Angeles" style="width:80%;">
-        </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="C:\Users\chhul\Pictures/Kristine.jpg" alt="Los Angeles" style="width:70%;">
+            <div>
+		      <p>Kristine</p>
+			</div>
+		  </div>
 
-        <div class="item">
-          <img src="/Ashley.jpg" alt="Chicago" style="width:80%;">
-        </div>
+          <div class="item">
+            <img src="C:\Users\chhul\Pictures/Ashley.jpg" alt="Chicago" style="width:70%;">
+		    <div>
+		      <p>Ashley</p>
+		    </div>
+          </div>
     
-        <div class="item">
-          <img src="/Katelynn3.jpg" alt="New york" style="width:80%;">
-        </div>
-		<div class="item">
-          <img src="/Scott1.JPG" alt="Chicago" style="width:80%;">
-        </div>
+          <div class="item">
+            <img src="C:\Users\chhul\Pictures/Katelynn3.jpg" alt="New york" style="width:70%;">
+		    <div>
+		      <p>Katelynn</p>
+			</div>
+          </div>
+		  
+		  <div class="item">
+            <img src="C:\Users\chhul\Pictures/Scott1.JPG" alt="Chicago" style="width:70%;">
+		    <div>
+		      <p>Scott</p>
+			</div>
+          </div>
     
-        <div class="item">
-          <img src="/Kristine.jpg" alt="New york" style="width:80%;">
-        </div>
-		<div class="item">
-          <img src="/Kristine.jpg" alt="New york" style="width:80%;">
-        </div>
+          <div class="item">
+            <img src="C:\Users\chhul\Pictures/Kristine.jpg" alt="New york" style="width:70%;">
+		    <div>
+		      <p>Sara</p>
+			</div>
+          </div>
 		
-      </div>
-	  <!-- Left and right controls -->
- <!--  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a> -->
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a> 
+		  <div class="item">
+            <img src="C:\Users\chhul\Pictures/Kristine.jpg" alt="New york" style="width:70%;">
+		    <div>
+		      <p>Emily</p>
+			</div>
+          </div>
+		
+        </div>
+	    <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a> 
+	  </div>
     </div>
-  </div>
-  </div>
-  </div>
-
- 
-   <br><br>
-   <div id="time">
+    </div> <br><br>
+    <div id="time">
      <?php
      date_default_timezone_set("America/New_York");
      echo "The time is " . date("h:i:sa");
      ?>
-   </div>
+    </div>
+  </div>
 </body>
 </html>
---
+++ web/homepageJs.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    document.getElementById("Me").innerHTML = "I am currently a Jumior at BYU-I." + 
+	"I am working towards achiveing my BS in Software Enginerring. " +
+	"I have been married for 25 years and have six children. ";
+}
