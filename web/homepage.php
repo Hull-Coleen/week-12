ab5f6b82e2b1843df1274c23255340b5e9c62b5a
chhull <ch.hull@verizon.net>
2018-05-04 11:08:32 -0400

    tried to make carousel work

--- web/homepage.php
+++ web/homepage.php
@@ -72,22 +72,22 @@
 		
       </div>
 	  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+ <!--  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
   </a>
   <a class="carousel-control-next" href="#myCarousel" data-slide="next">
     <span class="carousel-control-next-icon"></span>
-  </a>
+  </a> -->
 
       <!-- Left and right controls -->
-     <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
         <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#myCarousel" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right"></span>
         <span class="sr-only">Next</span>
-      </a> -->
+      </a> 
     </div>
   </div>
   </div>
