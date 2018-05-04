3714db7b7ed6ced87d58d050b2cffbf893991571
chhull <ch.hull@verizon.net>
2018-05-04 18:57:36 -0400

    fixed misspellings

--- web/homepageJs.js
+++ web/homepageJs.js
@@ -1,6 +1,7 @@
 
 function myFunction() {
     document.getElementById("Me").innerHTML = "I am currently a Junior at BYU-I." + 
-	"I am working towards achiveing my BS in Software Enginerring. " +
-	"I have been married for 25 years and have six children. ";
+	"I am working towards achieving my BS in Software Engineering. " +
+	"I have been married for 25 years and have six children. The picures cycling through" +
+	" on the right are my husband and children.";
 }
