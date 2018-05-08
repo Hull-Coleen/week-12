fae872946effb1e16e9b3805699b0d81d24d0ba3
chhull <ch.hull@verizon.net>
2018-05-08 11:24:59 -0400

    added echo

--- web/Team_week03.php
+++ web/Team_week03.php
@@ -87,13 +87,12 @@ function test_input($data) {
   
   <?php
   $continents = array("NA" => "North Amercian", "SA" => "South America", "EU" => "Europe", "AS" =>  "Asia", "AU" => "Australia", "AF" => "Africa", "AN" => "Antarctica");
-  foreach($continents as $key => $value)
-  for ($i = 0; $i < count($continents); $i++) {
-  echo <input type="checkbox" name="continent[]" value=$key>$value<br>;
+  foreach($continents as $key => $value) {
+  echo '<input type="checkbox" name="continent[]" value=$key>$value<br>';
 	  
   }
 	  
-  }
+  
   
   ?>
   Continents:<br>
