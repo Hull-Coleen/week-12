50cb01faf4c99309a62cd1a7bc33f22908f2f23f
chhull <ch.hull@verizon.net>
2018-05-10 12:30:11 -0400

    added cart

--- web/checkout.php
+++ web/checkout.php
@@ -8,6 +8,7 @@ session_start();
 </head>
 <body>
 <a href="/cart.php">Cart</a>
+<form method="POST" action="confirmation.php">
 <p>
 <label for="name">Name</label>
 			<input type="text" placeholder="Name" id="name" name="name">
@@ -16,7 +17,14 @@ session_start();
 			<label for="email">Email</label>
 			<input type="text" placeholder="Email Address" id="email" name="email">
 			<br />
+			<label for="email">Adress</label>
+			<input type="text" placeholder="Address" id="address" name="adress">
+			<br />
 			</p>
+			<input type="submit" value="Submit Answers">
+
+
+		</form>
 
 
 
--- web/confirmation.php
+++ web/confirmation.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+$_SESSION["name"] = htmlspecialchars($_POST["name"]);
+$_SESSION["email"] = htmlspecialchars($_POST["email"]);
+$_SESSION["adress"] = htmlspecialchars($_POST["address"]);
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,8 +12,9 @@ session_start();
 <title>Confirmation Page</title>
 </head>
 <body>
-
-
+<p>Your name is: <?=$name ?></p>
+	<p>Your email is: <a href="mailto:<?=$email ?>"><?=$email ?></a></p>
+	<p>Your adress is: <?=$address ?></p>
 <?php
 // remove all session variables
 session_unset(); 
