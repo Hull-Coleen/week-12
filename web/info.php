1d4883f073ba7c566cf7d32d9a86c8b9ec079e1c
chhull <ch.hull@verizon.net>
2018-05-11 15:32:45 -0400

    push seems to be working now

--- web/info.php
+++ web/info.php
@@ -82,9 +82,7 @@ p {
 <a href="/cart.php">Cart</a>
 	<img src="<?php echo $img; ?>" style="margin:0px auto;display:block" alt="World"> 
 <?php
-<script>
- 
-</script>
+
 // array_push($_SESSION["cart"], $f);
 
  //foreach ($_SESSION["cart"] as $fl) {
