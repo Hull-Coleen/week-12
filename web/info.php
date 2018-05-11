911db3ecfc4e3045511c4d4874dceacacd7e7769
chhull <ch.hull@verizon.net>
2018-05-11 00:37:50 -0400

    tried to show what as purchased on confirmation page

--- web/info.php
+++ web/info.php
@@ -17,7 +17,7 @@ session_start();
 
  echo $_SESSION["item"];
  echo $_SESSION["num"];
-$_SESSION["cart"] += ($_SESSION["item"] => $_SESSION["num"]);
+$_SESSION["cart"] += [$_SESSION["item"] => $_SESSION["num"]];
  $_SESSION["cart"] = array_push_assoc($_SESSION["cart"], $_SESSION["item"] , $_SESSION["num"]);
  //$_SESSION["cart"]_push($$_SESSION["item"], $_SESSION["num"]);
  //array_push($data,$question);
