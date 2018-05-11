c4e5c24a916416e00445f0adbfc9cc280dcdfabe
chhull <ch.hull@verizon.net>
2018-05-11 14:20:31 -0400

    tried to show what as purchased on confirmation page

--- web/info.php
+++ web/info.php
@@ -27,57 +27,18 @@ else {
 <a href="/cart.php">Cart</a>
 	<img src="<?php echo $img; ?>" style="margin:0px auto;display:block" alt="World"> 
 <?php
-//$a = $_SESSION["cart"];
 
-	
-	//$_SESSION["num"] = $_POST["num"];
 
  echo $_SESSION["item"];
- //echo "t" . $t . "f" . $f . "<br>"; 
- //echo $_SESSION["num"];
- $t = $_SESSION["item"];
- //$test = array($_SESSION["item"] => $_SESSION["num"]);
- //$result = array_merge($test, $_SESSION["cart"]);
- //foreach ($test as $key => $value) {
-	// echo $key;
-	 //echo $value;
-	 
- //}
- 
- //array_push($a, $f);
- //$_SESSION["cart"] = $a;
- //array_merge($a,$_SESSION["cart"]);
- 
- foreach ($a as $fl) {
-	 echo "session " . htmlspecialchars($fl);
- }
- //array_push(echo "<br><br>" . count($_SESSION["cart"]);, $a);
+
  array_push($_SESSION["cart"], $f);
- //array_push($_SESSION["cart"], $_SESSION["item"]);
+
  foreach ($_SESSION["cart"] as $fl) {
 	 echo "session " . htmlspecialchars($fl);
  }
  
  echo "<br><br>" . count($_SESSION["cart"]);
  
-  //$_SESSION["cart"][$f] = "1";
-  //
- //array_push($a, $f -> "1");
- //$_SESSION["cart"][$t] = '1';
-  //$_SESSION["cart"] = $a;
-//array_push($_SESSION["cart"], $f -> $_SESSION["num"]);
-////array_push($_SESSION["cart"], $test);
-//echo "session" . count($_SESSION["cart"]);
-//foreach ($_SESSION["cart"] as $fl => $value)
-//{
-	//echo htmlspecialchars($fl);
-	//echo "<br>";
-    //echo htmlspecialchars($value);
-	
-//}
-//echo "<br><br>" . count($_SESSION["cart"]);
-//echo count($result);
-//echo count($a);
  
 
 ?>
