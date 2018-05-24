ee77d833072a4aa2357dcd3d424c34a92f1c6fc0
chhull <ch.hull@verizon.net>
2018-05-24 13:09:28 -0400

    info page

--- web/info05.php
+++ web/info05.php
@@ -6,14 +6,14 @@ $_SESSION["item"] = $_POST["item"];
 $description = $_SESSION["item"];
 $price;
 $image = "world.jpg";
-$statement = $db->query("SELECT flower_price, image FROM flower WHERE description = $description");
+$statement = $db->query("SELECT flower_price FROM flower WHERE description = $description)");
 $statement->execute();
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	$price = $row['flower_price'];
-	$image = $row['image'];
+$price = $statement->fetch()['flower_price'];
+
+	//$price = $row['flower_price'];
+	//$image = $row['image'];
 	
-}
+
 //$price = $statement->fetch()['flower_price'];
 //$image = $statement->fetch()['image'];
 ?>
