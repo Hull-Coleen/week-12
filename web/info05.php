23c76c43938417f851baa9d44e5cd8d9d5daf4cd
chhull <ch.hull@verizon.net>
2018-05-24 14:01:58 -0400

    info page

--- web/info05.php
+++ web/info05.php
@@ -5,7 +5,7 @@ include_once('/dbConnect.php');
 $_SESSION["item"] = $_POST["item"];
 $description = $_SESSION["item"];
 $price;
-$image = "world.jpg";
+$image = "world.jpg";    
 //$stmt = $db->prepare("SELECT flower_price, image FROM flower WHERE (description = '{$description}');");
 //$stmt->execute();
 //$v = $stmt->fetchAll(PDO::FETCH_ASSOC);
