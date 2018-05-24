1939b2eed7d6121c93ef3f1fd503a3b4be4a91ab
chhull <ch.hull@verizon.net>
2018-05-24 19:32:02 -0400

    deleted code from search page

--- web/search.php
+++ web/search.php
@@ -1,10 +1,5 @@
 <?php
-session_start();
-include_once('dbConnect.php');
-$occasion = htmlspecialchars($_POST["occasion"]);
-$stmt = $db->prepare("SELECT flower_price, description, image FROM flower WHERE (flower_type = '{$occasion}');");
-$stmt->execute();
-//$price = $stmt->fetch()['flower_price'];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,29 +9,6 @@ $stmt->execute();
 </head>
 <body>
 
-<h1>Please Enter your search criteria</h1>
-<form method="POST" action=""<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"">
-<p>
-  <label for="name">Occasion</label>
-  <input type="text" placeholder="Occasion" id="occasion" name="occasion">
-			
-  <br /><br />
-  <input type="submit" value="Complete Checkout">
-</p>
-</form>
-<?php
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-	?>
-	<div id="flowers" ><p>
-     <img src="<?php echo $row['image'] ?>" alt="Flower"> <br>
-     <input type="radio" name="item" value="<?php echo $row['description'] ?>">
-	 <?php echo $row['description'] ?><br />
-	 <?php echo $row['flower_price'] ?></P> 
-   </div>
-<?php
-}
-?>
 
 
 </body>
