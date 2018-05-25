723f7e1d8ae03a17d7435e3e0f750dc13f362de0
chhull <ch.hull@verizon.net>
2018-05-24 22:53:11 -0400

    update php

---
+++ web/cart05.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+$t = $_GET['_delete'];
+if ($t > -1) {
+	unset($_SESSION["cart"][$t]);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type="text/css" href="Prove03style.css">
+</head>
+<body>
+<a href="/Week05.php">Continue Shopping</a><br>
+<a href="/checkout.php">Checkout</a>
+<h1>Shopping Cart</h1><br><br>
+
+<?php
+for ($i = 0; $i < count($_SESSION['cart']); $i++) {
+	?>
+	<p> <?php
+   echo $_SESSION["cart"][$i]; 
+   ?>
+   <br>
+   <input type="text" placeholder="1" maxlength="4" size="4" id="num" name="num">
+   <?php
+   echo "<br>";
+   echo "<a href='{$_SERVER["PHP_SELF"]}?_delete={$i}'>Delete</a>";
+   ?>
+   </p>
+   <?php
+}
+?>
+
+</body>
+</html>
