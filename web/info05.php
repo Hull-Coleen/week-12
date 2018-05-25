fcf3bc033206155edcd7cad4af0d29dfd75f1e73
chhull <ch.hull@verizon.net>
2018-05-24 23:16:43 -0400

    update php

--- web/info05.php
+++ web/info05.php
@@ -31,8 +31,6 @@ $image = $stmt2->fetch()['image'];
  <?php 
  $num = 1;
  if (!empty(htmlspecialchars($_POST["item1"]))) {
-	 
-    $_SESSION["cart"] += array($_POST["item1"] => 1);
 	array_push($_SESSION["cart"], $_POST["item1"] -> $num);
  }
  echo  $_POST["item1"];
