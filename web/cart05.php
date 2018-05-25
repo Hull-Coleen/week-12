037ad70d6a8543bd195224f2641f37b8748cc618
chhull <ch.hull@verizon.net>
2018-05-25 09:59:57 -0400

    working on deleting from assoc array

--- web/cart05.php
+++ web/cart05.php
@@ -14,23 +14,23 @@ if ($t > -1) {
 <link rel="stylesheet" type="text/css" href="Prove03style.css">
 </head>
 <body>
-<a href="/Week05.php">Continue Shopping</a><br>
+<a href="Week05.php">Continue Shopping</a><br>
 <a href="/checkout.php">Checkout</a>
 <h1>Shopping Cart</h1><br><br>
 
 <?php
-for ($i = 0; $i < count($_SESSION['cart']); $i++) {
-//foreach($_SESSION['cart'] as $x => $x_value) {	
+//for ($i = 0; $i < count($_SESSION['cart']); $i++) {
+foreach($_SESSION['cart'] as $x => $x_value) {	
 	?>
 	<p> <?php
-  // echo $x . "  " . $x_value;
-   echo $_SESSION['cart'][$i] . ' ' . $array[$_SESSION['cart'][$i]] . "\n";   
+   echo $x;
+   //echo $_SESSION['cart'][$i] . ' ' . $array[$_SESSION['cart'][$i]] . "\n";   
    ?>
    <br>
    <input type="text" placeholder="1" maxlength="4" size="4" id="num" name="num">
    <?php
    echo "<br>";
-   echo "<a href='{$_SERVER["PHP_SELF"]}?_delete={$i}'>Delete</a>";
+   echo "<a href='{$_SERVER["PHP_SELF"]}?_delete={$x}'>Delete</a>";
    ?>
    </p>
    <?php
