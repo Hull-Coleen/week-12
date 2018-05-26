198a631ce3575696dd2ce0d9793b12a7282fba01
chhull <ch.hull@verizon.net>
2018-05-26 17:07:49 -0400

    trying new way to use dbConnect

--- web/Week05confirm.php
+++ web/Week05confirm.php
@@ -1,9 +1,22 @@
 <?php
 session_start();
-//include_once('dbConnect.php');
+include_once('dbConnect.php');
 $_SESSION["name"] = htmlspecialchars($_POST["name"]);
 $_SESSION["email"] = htmlspecialchars($_POST["email"]);
 $_SESSION["address"] = htmlspecialchars($_POST["address"]);
+$name = htmlspecialchars($_POST["username"]);
+$password = htmlspecialchars($_POST["email"]);
+$stmt = $db->prepare("SELECT username FROM public.user WHERE (user_user_name = '{$name}')
+AND password = '{$password}';");
+$stmt->execute();
+$userName = $stmt->fetch()['user_name'];
+if (empty(htmlspecialchars($_POST["name"]))  && !empty(htmlspecialchars($userName))) {
+	$name1 = u $userName;
+}
+else {
+	$name1 = $_SESSION["name"];
+}
+	
  
 ?>
 <!DOCTYPE html>
@@ -13,7 +26,7 @@ $_SESSION["address"] = htmlspecialchars($_POST["address"]);
 <link rel="stylesheet" type="text/css" href="Week05style.css">
 </head>
 <body>
-    <p id="confirm" > <?php echo "Thank you for your purchase: " . $_SESSION["name"] ?></p>
+    <p id="confirm" > <?php echo "Thank you for your purchase: " . $name1 ?></p>
 	<p id="confirm" >Your purchase will be shippped to: <?php echo $_SESSION["address"] ?></p>
 	<p id="confirm" >We will email you a confirmation number at: <?php echo $_SESSION["email"] ?></p>
 	
