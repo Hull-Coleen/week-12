10287466d95ade6e0225c3bb6dad13248a5f02a2
chhull <ch.hull@verizon.net>
2018-05-28 17:25:29 -0400

    moving things around

--- web/Week05checkout.php
+++ web/Week05checkout.php
@@ -12,7 +12,7 @@ include_once('dbConnect.php');
 <a href="/cart05.php">Back to Cart</a><br>
 
 <div id="row">
-<h1>If you have an account, Please sign in</h1><br>
+<h1>If you have an account,<br> Please sign in</h1><br>
 <form method="POST" action="Week05confirm.php">
 <p>
   
@@ -20,7 +20,7 @@ include_once('dbConnect.php');
   <input type="text" placeholder="Name" id="username" name="username"><br><br>
   <label for="password">Password</label>
   <input type="text" placeholder="Password" id="password" name="password"><br><br>
-  <br><br><br><br><br><br><br><input type="submit" value="Complete Checkout">
+  <br><br><br><br><br><br><input type="submit" value="Complete Checkout">
   </p>
 </form>
 </div>
