fb80625c9805888af42cdce980d37af17d53efc9
chhull <ch.hull@verizon.net>
2018-05-30 09:19:37 -0400

    week 6 pages

---
+++ prove/Week06confirm.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+include_once('dbConnect.php');
+$_SESSION["name"] = htmlspecialchars($_POST["name"]);
+$_SESSION["email"] = htmlspecialchars($_POST["email"]);
+$_SESSION["address"] = htmlspecialchars($_POST["address"]);
+$name = htmlspecialchars($_POST["username"]);
+$password = htmlspecialchars($_POST["password"]);
+
+$stmt = $db->prepare("SELECT user_name FROM public.user WHERE (user_user_name = '{$name}')
+AND user_password = '{$password}';");
+$stmt->execute();
+$userName = $stmt->fetch()['user_name'];
+
+if (htmlspecialchars($_POST["name"]) == "") {
+	$name1 = $userName;
+}
+else {
+	$name1 = $_SESSION["name"];
+}
+	
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Confirmation Page</title>
+<link rel="stylesheet" type="text/css" href="Week05style.css">
+</head>
+<body>
+    <p id="confirm" > <?php echo "Thank you for your purchase: " . $name1 ?></p>
+	<p id="confirm" >Your purchase will be shippped to: <?php echo $_SESSION["address"] ?></p>
+	<p id="confirm" >We will email you a confirmation number at: <?php echo $_SESSION["email"] ?></p>
+	
+<?php
+	foreach ($_SESSION["cart"] as $fl => $value)
+    {
+	?>
+	<p id="confirm"> <?php echo htmlspecialchars($fl); ?> </p>
+	<?php
+	}
+    // remove all session variables
+    session_unset(); 
+
+    // destroy the session 
+    session_destroy(); 
+?>
+</body>
+</html>
