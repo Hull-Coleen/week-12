7e9aedcb2d8fa8c93a9a89bf76411ce017c69ac9
chhull <ch.hull@verizon.net>
2018-06-01 20:40:33 -0400

    correct variable

--- web/Prove/Week06confirm.php
+++ web/Prove/Week06confirm.php
@@ -19,16 +19,7 @@ foreach ($user as $u) { ?>
 <?php	
 	
 }
-if(isset($_POST['submit'])) {
-    echo "Thanks for your purchase";
-    deleteUserCart($_SESSION['id']);
-	// remove all session variables
-    session_unset(); 
 
-    // destroy the session 
-    session_destroy();	
-                
-}
 foreach ($cart as $c) { ?>
 	<div ><p>
       <img id="cart" src="<?php echo $c['image'] ?>" alt="Flower">
@@ -40,7 +31,17 @@ foreach ($cart as $c) { ?>
 	<?php
 }
 
- 
+ if(isset($_POST['submit'])) {
+    echo "Thanks for your purchase";
+    deleteUserCart($_SESSION['id']);
+	// remove all session variables
+    session_unset(); 
+
+    // destroy the session 
+    session_destroy();	
+	echo $_SESSION['id'];
+                
+}
 ?>
 <form method="POST" action=""<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"">
 <p><input type="submit" name="submit" value="Confirm Transaction"></p>
