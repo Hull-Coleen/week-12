a3ed6f9f0ff7a8014c7b6ba16b70c372d1abb700
chhull <ch.hull@verizon.net>
2018-06-06 10:37:44 -0400

    changed search to go to info instead of adding to cart

--- web/week07/SignIn.php
+++ web/week07/SignIn.php
@@ -47,19 +47,19 @@ if (isset($_POST)) {
 		  }
 		  else {
 		     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-			 $id = setUser($name, $username, $hashedPassword, $address, $email);
+			 //$id = setUser($name, $username, $hashedPassword, $address, $email);
 			 //$id1 = getUserId($username);
 			 //if(!empty($checkPassword)) {
-			     if (!empty($id)) {
-					 $_SESSION["id"] = $id;
-					 $_SESSION['name'] = $username;
-                     header('Location: Week06.php');
-				     die();
-                 }
-                 else {
-					 $id = "";
-					 $error = "unable to create account, Please reenter your information";
-                 }					 
+			   //  if (!empty($id)) {
+				//	 $_SESSION["id"] = $id;
+					// $_SESSION['name'] = $username;
+                    // header('Location: Week06.php');
+				     //die();
+                // }
+                // else {
+					// $id = "";
+					 //$error = "unable to create account, Please reenter your information";
+                 //}					 
 			  //}
 		  }
 		
