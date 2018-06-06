00c9d18b2d4f3a784d13e20bda267572b54b0d10
chhull <ch.hull@verizon.net>
2018-06-06 13:04:01 -0400

    changed search to go to info instead of adding to cart

--- web/week07/prove.js
+++ web/week07/prove.js
@@ -3,8 +3,12 @@ function check(password1, password2) {
     document.getElementById('password2').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
+	 document.getElementById('message1').style.color = 'green';
+    document.getElementById('message1').innerHTML = 'matching';
   } else {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'not matching';
+	document.getElementById('message1').style.color = 'red';
+    document.getElementById('message1').innerHTML = 'not matching';
   }
 }
