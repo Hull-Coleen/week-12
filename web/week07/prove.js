99274e5ad6f97760fd3e5c9fee37eb0c1d054d74
chhull <ch.hull@verizon.net>
2018-06-09 11:04:15 -0400

    tyring to add some more javascript for assignment

--- web/week07/prove.js
+++ web/week07/prove.js
@@ -1,12 +1,10 @@
-function check(password1, password2) {
+function check(password1, password2, message, message1) {
   if (document.getElementById('password1').value ==
     document.getElementById('password2').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
 	 document.getElementById('message1').style.color = 'green';
     document.getElementById('message1').innerHTML = 'matching';
-	 document.getElementById('message2').style.color = 'green';
-    document.getElementById('message2').innerHTML = 'matching';
   } else {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'not matching';
