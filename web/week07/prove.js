ed2ec648d22b96cdd4512f562f32926297f2535c
chhull <ch.hull@verizon.net>
2018-06-09 12:56:12 -0400

    tyring to add some more javascript for assignment

--- web/week07/prove.js
+++ web/week07/prove.js
@@ -15,7 +15,7 @@ function signinCheck() {
 function check() {
 	var pattern = new RegExp('/^(?=\D*\d)[^ ]{6,}$/');
 	if (pattern.test(password)) {
-		document.getElementById('passwordText').innerHTML = 'working';
+		document.getElementById('passwordText').style.visibility="hidden";
 	}
 	else {
 	    document.getElementById('passwordText').innerHTML = 'Password must have one number and be at least 7 characters';
